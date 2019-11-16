9380e206aea879c8cf602cb31b1d20b914de9f06
timmysutanto <13517087@std.stei.itb.ac.id>
2019-11-16 20:21:56 +0700

    added cards

--- frontend-web/src/detail.js
+++ frontend-web/src/detail.js
@@ -4,6 +4,10 @@ import React from 'react';
 import { Container, Row, Col,Card,CardBody } from 'reactstrap';
 import PieChartDetail from './pieChart'
 import LineChartDetail from './lineChart'
+import check from './asset/check.png'
+import stop from './asset/stop.png'
+import truck from './asset/truck.png'
+import wrench from './asset/tools.png'
 
 
 class Detail extends React.Component {
@@ -53,9 +57,67 @@ class Detail extends React.Component {
               </Card>
             </Col>
             <Col>
-            <PieChartDetail />
-            <hr style={{border: '1px solid gray'}} />
-            <LineChartDetail />
+              <PieChartDetail />
+              <Row>
+                <Col>
+                  <Card style = {{width: '100%' , margin:'1rem','border':'2px solid','padding':'none'}}>
+                    <CardBody style ={{backgroundColor:'red'}}>
+                      <tr>
+                        <td>
+                          <img src={stop} width='30rem' />
+                        </td>
+                        <td>
+                          <h3 style={{ color:'white'}}><b>78</b></h3>
+                        </td>
+                      </tr>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card style = {{width: '100%' , margin:'1rem','border':'2px solid','padding':'none'}}>
+                    <CardBody style ={{backgroundColor:'red'}}>
+                      <tr>
+                        <td>
+                          <img src={wrench} width='30rem' />
+                        </td>
+                        <td>
+                          <h3 style={{ color:'white'}}><b>78</b></h3>
+                        </td>
+                      </tr>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card style = {{width: '100%' , margin:'1rem','border':'2px solid','padding':'none'}}>
+                    <CardBody style ={{backgroundColor:'red'}}>
+                      <tr>
+                        <td>
+                          <img src={truck} width='30rem' />
+                        </td>
+                        <td>
+                          <h3 style={{ color:'white'}}><b>78</b></h3>
+                        </td>
+                      </tr>
+                    </CardBody>
+                  </Card>
+                </Col>
+                <Col>
+                  <Card style = {{width: '100%' , margin:'1rem','border':'2px solid','padding':'none'}}>
+                    <CardBody style ={{backgroundColor:'red'}}>
+                      <tr>
+                        <td>
+                          <img src={check} width='30rem' />
+                        </td>
+                        <td>
+                          <h3 style={{ color:'white'}}><b>78</b></h3>
+                        </td>
+                      </tr>
+                    </CardBody>
+                  </Card>
+                </Col>
+              </Row>
+              <hr style={{border: '1px solid gray'}} />
+              <LineChartDetail />
             </Col>
           </Row>
         </Container>
