89f5e7b9f8907f0556ae71c0eded836b3c62bf18
timmysutanto <13517087@std.stei.itb.ac.id>
2019-11-16 15:50:38 +0700

    navbar styling

--- frontend-web/src/navbar.js
+++ frontend-web/src/navbar.js
@@ -1,25 +1,22 @@
 import React from 'react'
 import {
-    Navbar, Nav, NavLink, NavbarBrand, Form, Button
+    Navbar, Nav, NavLink, NavbarBrand, Form
   } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import logo from './asset/bulzen.png'
 class Navibar extends React.Component {
     render() {
       return (
         <div>
-            {/* <h1>Navbar</h1> */}
-
-            <Navbar bg="dark" variant="dark">
-                <NavbarBrand href="#home">Navbar</NavbarBrand>
+            {/* <Navbar fixed="top" bg="dark"  variant="dark"> */}
+            <Navbar fixed="top" style={{backgroundColor : '#343a40'}}>
+                <NavbarBrand href="/" style={{color : '#FFFFFF', fontSize:30, fontWeight:"bold", fontStyle:'italic'}}>BULZEN</NavbarBrand>
                 <Nav className="mr-auto">
-                <NavLink href="#home">Home</NavLink>
-                <NavLink href="#features">Features</NavLink>
-                <NavLink href="#pricing">Pricing</NavLink>
+                <NavLink href="/heavy" style={{color : '#8F9396'}}>Home</NavLink>
+                <NavLink href="#features" style={{color : '#8F9396'}}>Features</NavLink>
+                <NavLink href="#pricing" style={{color : '#8F9396'}}>Pricing</NavLink>
                 </Nav>
                 <Form inline>
-                {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
-                <Button variant="outline-info">Search</Button>
                 </Form>
             </Navbar>
         </div>
